5d52055145da7235f6ffa51a91fa661a66d277fc
Frank Mathy <frankmathy@gmail.com>
2020-10-04 23:17:34 +0200

    Show list of users

--- src/users/UserList.js
+++ src/users/UserList.js
@@ -2,7 +2,23 @@ import React, { useState, useEffect } from 'react';
 import './UserList.css';
 import axios from '../api/axios';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650
+  }
+});
+
 function UserList() {
+  const classes = useStyles();
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -16,11 +32,28 @@ function UserList() {
   return (
     <div>
       <h1>Userlist</h1>
-      {users.map(user => (
-        <p>
-          {user.firstName} {user.lastName}
-        </p>
-      ))}
+      <TableContainer component={Paper}>
+        <Table className={classes.table} size="small" aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Email</TableCell>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map(user => (
+              <TableRow key={user.id}>
+                <TableCell component="th" scope="row">
+                  {user.email}
+                </TableCell>
+                <TableCell>{user.firstName}</TableCell>
+                <TableCell>{user.lastName}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
